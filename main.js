0e005f21c713cbce633d4f5126823ec68fbcf707
Manuel Valls Fernández <manolo@vlrz.es>
2015-10-11 17:12:49 +0200

    Hotfix

--- main.js
+++ main.js
@@ -7,21 +7,21 @@ else if(global.TextDecoder) module.exports = function(data){
   if(!data.BYTES_PER_ELEMENT) data = new Uint8Array(data);
   return (new TextDecoder('utf-8')).decode(data);
 };
-else module.exports = function(data){
+else module.exports = function(bytes){
   var ret = '',
       i,
       code,
       next;
-  
-  if(!data.BYTES_PER_ELEMENT) data = new Uint8Array(data);
-  
+
+  if(!bytes.BYTES_PER_ELEMENT) bytes = new Uint8Array(bytes);
+
   for(i = 0;i < bytes.length;i++){
-    
+
     if(bytes[i] < 128){
       ret += String.fromCharCode(bytes[i]);
       continue;
     }
-    
+
     if(bytes[i] < 224){
       code = (bytes[i] & 0x3f) << 6;
       next = i + 1;
@@ -38,15 +38,14 @@ else module.exports = function(data){
       code = (bytes[i] & 0x03) << 30;
       next = i + 5;
     }
-    
+
     do{
       i++;
       code |= (bytes[i] & 0x7f) << ((next - i) * 6);
     }while(i != next);
-    
+
     ret += String.fromCharCode(code);
   }
-  
+
   return ret;
 };
-
